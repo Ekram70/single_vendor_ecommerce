3b9d61a5c78385132b38fa6de562170387ee9495
Mahmudtareq <tareqlpu5@gmail.com>
2023-01-01 23:10:36 +0600

    feat: Error, Otp, Forgetpass and  Resetpass Components added

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import { ColorSchemeProvider, MantineProvider } from "@mantine/core";
 import { useLocalStorage } from "@mantine/hooks";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import mantineThemeObj from "../config/mantineThemeObj";
+import Error from "./components/Error/Error";
+import ForgetPassword from "./components/ForgetPassword/ForgetPassword";
 import Login from "./components/Login/Login";
 import MainHeader from "./components/MainHeader/MainHeader";
+import OtpComponents from "./components/OtpComponents/OtpComponents";
 import Register from "./components/Register/Register";
+import ResetPassword from "./components/ResetPassword/ResetPassword";
 
 function App() {
   const [colorScheme, setColorScheme] = useLocalStorage({
@@ -30,7 +34,11 @@ function App() {
           <Routes>
             <Route path="/" element={<MainHeader />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/resetpassword" element={<ResetPassword />} />
+            <Route path="/forgetpassword" element={<ForgetPassword />} />
+            <Route path="/otpcomponents" element={<OtpComponents />} />
             <Route path="/register" element={<Register />} />
+            <Route path="*" element={<Error />} />
           </Routes>
         </BrowserRouter>
       </MantineProvider>
---
+++ src/components/Error/Error.jsx
@@ -0,0 +1,46 @@
+import { BackgroundImage, useMantineColorScheme } from "@mantine/core";
+import React from "react";
+import bgImage from "../../assets/images/pattern.jpg";
+import MainHeader from "../MainHeader/MainHeader";
+
+const Error = () => {
+  const { colorScheme } = useMantineColorScheme();
+  const dark = colorScheme === "dark";
+  return (
+    <BackgroundImage
+      src={bgImage}
+      className={` bg-left-top bg-repeat bg-blend-multiply 2xl:bg-auto`}
+    >
+      <MainHeader />
+      <div className="h-screen w-full bg-gray-800">
+        <div className="flex items-center justify-center py-12">
+          <div className="mx-4 flex w-full items-center justify-center rounded-md border bg-white md:w-2/3">
+            <div className="flex flex-col items-center py-16 ">
+              <img
+                className="hidden px-4 md:block"
+                src="https://i.ibb.co/9Vs73RF/undraw-page-not-found-su7k-1-3.png"
+                alt=""
+              />
+              <img
+                className="md:hidden"
+                src="https://i.ibb.co/RgYQvV7/undraw-page-not-found-su7k-1.png"
+                alt=""
+              />
+              <h1 className="px-4 pt-8 pb-4 text-center text-5xl font-bold leading-10 text-gray-800">
+                OOPS!{" "}
+              </h1>
+              <p className="px-4 pb-10 text-center text-base leading-none text-gray-600">
+                No signal here! we cannot find the page you are looking for{" "}
+              </p>
+              <button className="mx-4 h-10 w-44 rounded-md border bg-indigo-700 text-base text-white hover:bg-indigo-800 focus:outline-none focus:ring-2 focus:ring-indigo-800 focus:ring-opacity-50">
+                Go Back
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </BackgroundImage>
+  );
+};
+
+export default Error;

0227c29ff9228d4bd14427302a94fd0fa483e163
Mahmudtareq <tareqlpu5@gmail.com>
2023-01-11 23:09:02 +0600

    feat: sidenavbar, userordertable, ordercard and dashbordheader are added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useColorScheme, useLocalStorage } from "@mantine/hooks";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import mantineThemeObj from "../config/mantineThemeObj";
 import Error from "./components/Error/Error";
+import AdmindashBord from "./pages/AdmindashBord";
 import ForgotPasswordPage from "./pages/ForgotPasswordPage";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
@@ -39,6 +40,7 @@ function App() {
             <Route path="/forget" element={<ForgotPasswordPage />} />
             <Route path="/otp" element={<OtpPage />} />
             <Route path="/register" element={<RegisterPage />} />
+            <Route path="/admindashbord" element={<AdmindashBord />} />
             <Route path="*" element={<Error />} />
           </Routes>
         </BrowserRouter>

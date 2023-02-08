20d9750322f5b9aaf2c9feb2627437103f709e09
Ekram70 <ekramullah70@gmail.com>
2023-02-08 17:46:17 +0600

    refactor: user reviews and wishlist responsive

--- src/components/UserReviews/UserReviews.jsx
+++ src/components/UserReviews/UserReviews.jsx
@@ -1,23 +1,11 @@
-import {
-  Anchor,
-  Box,
-  Group,
-  Header,
-  Paper,
-  Rating,
-  Text,
-  Title
-} from '@mantine/core';
+import { Anchor, Box, Group, Paper, Rating, Text, Title } from '@mantine/core';
 import { IconEdit, IconTrash } from '@tabler/icons';
 import React from 'react';
 
 const UserReviews = () => (
-  <Box component="main" className="my-5 px-2 pt-5 lg:basis-[78%]">
-    <Box
-      component="div"
-      className="my-4 rounded-lg border border-solid border-gray-600 p-4 shadow-md"
-    >
-      <Header className="h-14 w-full">
+  <Box component="main" className="my-5 space-y-5 px-2 pt-5 lg:basis-[78%]">
+    <Box>
+      <Paper component="div" className="rounded-none p-4" withBorder>
         <Group position="apart" className="mt-3 px-4">
           <Group>
             <Title order={5}>
@@ -40,8 +28,8 @@ const UserReviews = () => (
             </Anchor>
           </Group>
         </Group>
-      </Header>
-      <Paper className="p-4">
+      </Paper>
+      <Paper className="rounded-none border-t-0 p-4" withBorder>
         <Group className="my-2">
           <Rating defaultValue={5} />
         </Group>
@@ -55,51 +43,9 @@ const UserReviews = () => (
         </Group>
       </Paper>
     </Box>
-    <Box
-      component="div"
-      className="my-4 rounded-lg border border-solid border-gray-600 p-4 shadow-md"
-    >
-      <Header className="h-14 w-full">
-        <Group position="apart" className="mt-3 px-4">
-          <Group>
-            <Title order={5}>Product Name:</Title>
-            <span className="text-sm font-medium">Brand New Body Spray</span>
-          </Group>
-          <Group position="apart">
-            <Anchor>
-              <span className="flex items-center font-medium">
-                {' '}
-                <IconEdit size={18} className="mr-2" /> Edit
-              </span>
-            </Anchor>
-            <Anchor>
-              <span className="flex items-center font-medium">
-                {' '}
-                <IconTrash size={18} className="mr-2" /> Delete
-              </span>
-            </Anchor>
-          </Group>
-        </Group>
-      </Header>
-      <Paper className="p-4">
-        <Group className="my-2">
-          <Rating defaultValue={5} />
-        </Group>
-        <Group>
-          <Text className="text-justify">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa
-            excepturi perspiciatis incidunt iusto nesciunt similique libero
-            totam porro veniam ipsum iure, possimus ad sunt quam laborum quasi
-            animi, aut laudantium placeat.
-          </Text>
-        </Group>
-      </Paper>
-    </Box>
-    <Box
-      component="div"
-      className="my-4 rounded-lg border border-solid border-gray-600 p-4 shadow-md"
-    >
-      <Header className="h-14 w-full">
+
+    <Box>
+      <Paper component="div" className="rounded-none p-4" withBorder>
         <Group position="apart" className="mt-3 px-4">
           <Group>
             <Title order={5}>
@@ -122,8 +68,47 @@ const UserReviews = () => (
             </Anchor>
           </Group>
         </Group>
-      </Header>
-      <Paper className="p-4">
+      </Paper>
+      <Paper className="rounded-none border-t-0 p-4" withBorder>
+        <Group className="my-2">
+          <Rating defaultValue={5} />
+        </Group>
+        <Group>
+          <Text className="text-justify">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa
+            excepturi perspiciatis incidunt iusto nesciunt similique libero
+            totam porro veniam ipsum iure, possimus ad sunt quam laborum quasi
+            animi, aut laudantium placeat.
+          </Text>
+        </Group>
+      </Paper>
+    </Box>
+    <Box>
+      <Paper component="div" className="rounded-none p-4" withBorder>
+        <Group position="apart" className="mt-3 px-4">
+          <Group>
+            <Title order={5}>
+              Product Name:{' '}
+              <span className="text-sm font-medium">Brand New Body Spray</span>
+            </Title>
+          </Group>
+          <Group position="apart">
+            <Anchor>
+              <span className="flex items-center font-medium">
+                {' '}
+                <IconEdit size={18} className="mr-2" /> Edit
+              </span>
+            </Anchor>
+            <Anchor>
+              <span className="flex items-center font-medium">
+                {' '}
+                <IconTrash size={18} className="mr-2" /> Delete
+              </span>
+            </Anchor>
+          </Group>
+        </Group>
+      </Paper>
+      <Paper className="rounded-none border-t-0 p-4" withBorder>
         <Group className="my-2">
           <Rating defaultValue={5} />
         </Group>
--- src/components/WishList/WishList.jsx
+++ src/components/WishList/WishList.jsx
@@ -1,156 +1,117 @@
 import {
+  ActionIcon,
   Avatar,
   Badge,
   Box,
-  Checkbox,
   Group,
   Paper,
   Rating,
   Stack,
   Text
 } from '@mantine/core';
+import { useMediaQuery } from '@mantine/hooks';
 import { IconTrash } from '@tabler/icons';
 import React from 'react';
 import product1 from '../../assets/images/product4.png';
 
-const WishList = () => (
-  <Box component="main" className="w-full px-2 pt-5 lg:basis-[78%]">
-    <Box component="div">
-      <Group position="apart" className="rounded-md bg-yellow-400 px-6 py-4">
-        <Group>
-          <Checkbox
-            color="orange"
-            size="xs"
-            classNames={{ label: 'font-bold text-md' }}
-          />
-          <Text className="text-sm font-semibold">Product</Text>
-        </Group>
-        <Group position="apart" className=" lg:space-x-8">
-          <Text className="text-sm font-semibold">Price</Text>
-          <Text className="text-sm font-semibold">Status</Text>
-          <Text className="text-sm font-semibold">Action</Text>
-          <Text className="text-sm font-semibold">Remove</Text>
+const WishList = () => {
+  const isMobile = useMediaQuery('(min-width: 576px)');
+
+  return (
+    <Box component="main" className="w-full px-0 pt-5 md:px-2 lg:basis-[78%]">
+      <Group
+        position="apart"
+        className="justify-end gap-0 rounded-md bg-primary py-2 px-4 text-sm font-bold text-secondary"
+      >
+        <Text className="w-1/2">Product</Text>
+        <Group position="apart" className="sm:w-2/5">
+          <Text>Price</Text>
+          <Text>Status</Text>
+          <Text>Remove</Text>
         </Group>
       </Group>
-    </Box>
-    {/* wish card box */}
-    {/* <ScrollArea> */}
-    <Box component="section">
-      <Box component="div" className="my-3">
-        <Paper className="py-4 px-6 shadow-md">
-          <Group position="apart">
-            <Group>
-              <Checkbox size="xs" />
+
+      <Box component="section">
+        <Paper className="my-4 p-4" withBorder>
+          <Group position="apart" className={`${!isMobile && 'flex-col'}`}>
+            <Group className="justify-center gap-0 sm:w-1/2 sm:justify-start">
               <Avatar size="lg" src={product1} />
-              <Stack>
-                <Text className="text-sm font-normal">
+              <Stack className="items-center gap-0 sm:items-start">
+                <Text className="text-center text-sm font-semibold sm:text-start">
                   Field Roast Chao Cheese Creamy Original
                 </Text>
-                <Box className="flex items-center">
+                <Group>
                   <Rating defaultValue={4.5} size="sm" />
-                  <span className="ml-1 text-sm">4.95 out of 5</span>
-                </Box>
+                  <Text component="span" className="text-sm text-gray-400">
+                    (4.5)
+                  </Text>
+                </Group>
               </Stack>
             </Group>
-            <Group position="apart" className=" lg:space-x-8">
-              <Text className="text-sm font-semibold">$2.15</Text>
-              <Text className="text-sm font-semibold">
-                <Badge color="orange" variant="filled">
-                  Instock
-                </Badge>
-              </Text>
-              <Text className="text-sm font-semibold">
-                {' '}
-                <Badge color="orange" variant="filled">
-                  Pending
-                </Badge>
-              </Text>
-              <Text className="text-sm font-semibold">
+            <Group position="apart" className="sm:w-2/5">
+              <Text className="text-2xl font-semibold">$2.15</Text>
+              <Badge variant="filled">Instock</Badge>
+              <ActionIcon>
                 <IconTrash size={18} />
-              </Text>
+              </ActionIcon>
+            </Group>
+          </Group>
+        </Paper>
+
+        <Paper className="my-4 p-4" withBorder>
+          <Group position="apart" className={`${!isMobile && 'flex-col'}`}>
+            <Group className="justify-center gap-0 sm:w-1/2 sm:justify-start">
+              <Avatar size="lg" src={product1} />
+              <Stack className="items-center gap-0 sm:items-start">
+                <Text className="text-center text-sm font-semibold sm:text-start">
+                  Field Roast Chao Cheese Creamy Original
+                </Text>
+                <Group>
+                  <Rating defaultValue={4.5} size="sm" />
+                  <Text component="span" className="text-sm text-gray-400">
+                    (4.5)
+                  </Text>
+                </Group>
+              </Stack>
+            </Group>
+            <Group position="apart" className="sm:w-2/5">
+              <Text className="text-2xl font-semibold">$2.15</Text>
+              <Badge variant="filled">Instock</Badge>
+              <ActionIcon>
+                <IconTrash size={18} />
+              </ActionIcon>
+            </Group>
+          </Group>
+        </Paper>
+
+        <Paper className="my-4 p-4" withBorder>
+          <Group position="apart" className={`${!isMobile && 'flex-col'}`}>
+            <Group className="justify-center gap-0 sm:w-1/2 sm:justify-start">
+              <Avatar size="lg" src={product1} />
+              <Stack className="items-center gap-0 sm:items-start">
+                <Text className="text-center text-sm font-semibold sm:text-start">
+                  Field Roast Chao Cheese Creamy Original
+                </Text>
+                <Group>
+                  <Rating defaultValue={4.5} size="sm" />
+                  <Text component="span" className="text-sm text-gray-400">
+                    (4.5)
+                  </Text>
+                </Group>
+              </Stack>
+            </Group>
+            <Group position="apart" className="sm:w-2/5">
+              <Text className="text-2xl font-semibold">$2.15</Text>
+              <Badge variant="filled">Instock</Badge>
+              <ActionIcon>
+                <IconTrash size={18} />
+              </ActionIcon>
             </Group>
           </Group>
         </Paper>
       </Box>
     </Box>
-    <Box component="section">
-      <Box component="div" className="my-3">
-        <Paper className="py-4 px-6 shadow-md">
-          <Group position="apart">
-            <Group>
-              <Checkbox size="xs" />
-              <Avatar size="lg" src={product1} />
-              <Stack>
-                <Text className="text-sm font-normal">
-                  Field Roast Chao Cheese Creamy Original
-                </Text>
-                <Box className="flex items-center">
-                  <Rating defaultValue={4.5} size="sm" />
-                  <span className="ml-1 text-sm">4.95 out of 5</span>
-                </Box>
-              </Stack>
-            </Group>
-            <Group position="apart" className=" lg:space-x-8">
-              <Text className="text-sm font-semibold">$2.15</Text>
-              <Text className="text-sm font-semibold">
-                <Badge color="orange" variant="filled">
-                  Instock
-                </Badge>
-              </Text>
-              <Text className="text-sm font-semibold">
-                {' '}
-                <Badge color="orange" variant="filled">
-                  Pending
-                </Badge>
-              </Text>
-              <Text className="text-sm font-semibold">
-                <IconTrash size={18} />
-              </Text>
-            </Group>
-          </Group>
-        </Paper>
-      </Box>
-    </Box>
-    <Box component="section">
-      <Box component="div" className="my-3">
-        <Paper className="py-4 px-6 shadow-md">
-          <Group position="apart">
-            <Group>
-              <Checkbox size="xs" />
-              <Avatar size="lg" src={product1} />
-              <Stack>
-                <Text className="text-sm font-normal">
-                  Field Roast Chao Cheese Creamy Original
-                </Text>
-                <Box className="flex items-center">
-                  <Rating defaultValue={4.5} size="sm" />
-                  <span className="ml-1 text-sm">4.95 out of 5</span>
-                </Box>
-              </Stack>
-            </Group>
-            <Group position="apart" className=" lg:space-x-8">
-              <Text className="text-sm font-semibold">$2.15</Text>
-              <Text className="text-sm font-semibold">
-                <Badge color="orange" variant="filled">
-                  Instock
-                </Badge>
-              </Text>
-              <Text className="text-sm font-semibold">
-                {' '}
-                <Badge color="orange" variant="filled">
-                  Pending
-                </Badge>
-              </Text>
-              <Text className="text-sm font-semibold">
-                <IconTrash size={18} />
-              </Text>
-            </Group>
-          </Group>
-        </Paper>
-      </Box>
-    </Box>
-    {/* </ScrollArea> */}
-  </Box>
-);
+  );
+};
 
 export default WishList;
